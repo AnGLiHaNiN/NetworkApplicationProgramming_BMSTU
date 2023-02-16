9359a8eedcff8e62212a56115a8ba9082ef60491
AnGLiHaNiN <vaskin200333@gmail.com>
2023-02-16 18:02:51 +0300

    New Лаба 2: реализация функционала %

---
+++ Laboratory_1_2/script.js
@@ -0,0 +1,124 @@
+// файл script.js
+window.onload = function(){ 
+
+    let a = ''
+    let b = ''
+    let expressionResult = ''
+    let selectedOperation = null
+    
+    // окно вывода результата
+    outputElement = document.getElementById("result")
+    
+    // список объектов кнопок циферблата (id которых начинается с btn_digit_)
+    digitButtons = document.querySelectorAll('[id ^= "btn_digit_"]')
+    
+    function unaryOperation(){
+
+    }
+
+    function binaryOperation(){
+        
+    }
+
+
+    function calculate(){
+        switch(selectedOperation) { 
+            case 'x':
+                expressionResult = (+a) * (+b)
+                break;
+            case '+':
+                expressionResult = (+a) + (+b)
+                break;
+            case '-':
+                expressionResult = (+a) - (+b)
+                break;
+            case '/':
+                expressionResult = (+a) / (+b)
+                break;  
+        }
+ 
+        a = expressionResult.toString()
+        b = ''
+        selectedOperation = null
+    
+         
+
+    }
+    document.getElementById("btn_op_percent").onclick = function() {
+        if (a === ''){
+            return
+        } else if(a !== '' && b === ''){
+            expressionResult = (+a) / (100)
+            a = expressionResult.toString()
+    
+            outputElement.innerHTML = a
+        } else {
+            calculate()
+            expressionResult = (+a) / (100)
+            a = expressionResult.toString()
+    
+            outputElement.innerHTML = a
+
+        }
+    }
+    
+    function onDigitButtonClicked(digit) {
+        if (!selectedOperation) {
+            if ((digit != '.') || (digit == '.' && !a.includes(digit))) { 
+                a += digit
+            }
+            outputElement.innerHTML = a
+        } else {
+            if ((digit != '.') || (digit == '.' && !b.includes(digit))) { 
+                b += digit
+                outputElement.innerHTML = b        
+            }
+        }
+    }
+    
+    // устанавка колбек-функций на кнопки циферблата по событию нажатия
+    digitButtons.forEach(button => {
+        button.onclick = function() {
+            const digitValue = button.innerHTML
+            onDigitButtonClicked(digitValue)
+        }
+    });
+    
+    // установка колбек-функций для кнопок операций
+    document.getElementById("btn_op_mult").onclick = function() { 
+        if (a === '') return
+        selectedOperation = 'x'
+    }
+    document.getElementById("btn_op_plus").onclick = function() { 
+        if (a === '') return
+        selectedOperation = '+'
+    }
+    document.getElementById("btn_op_minus").onclick = function() { 
+        if (a === '') return
+        selectedOperation = '-'
+    }
+    document.getElementById("btn_op_div").onclick = function() { 
+        if (a === '') return
+        selectedOperation = '/'
+    }
+    
+    // кнопка очищения
+    document.getElementById("btn_op_clear").onclick = function() { 
+        a = ''
+        b = ''
+        selectedOperation = ''
+        expressionResult = ''
+        outputElement.innerHTML = 0
+    }
+
+
+    
+    // кнопка расчёта результата
+    document.getElementById("btn_op_equal").onclick = function() { 
+        if (a === '' || b === '' || !selectedOperation)
+            return
+           
+        calculate()
+        outputElement.innerHTML = a 
+    }
+    };
